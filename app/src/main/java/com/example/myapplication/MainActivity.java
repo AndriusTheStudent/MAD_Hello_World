4fc4b2ed0912613eac039e8c9e6141c11f161dfc
Andrius Markusenka <amarkusenka@gmail.com>
2019-09-15 19:38:48 +0100

    finished at lesson 01.2 slide 49

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,13 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Use class name as tag
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        final Button button = (Button) findViewById(R.id.showToast);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Show message in Android monitor, logcat pane
+        //Log.<log-level>(TAG, "Message");
         Log.d("Error Message", "lengthy error message");
+
+
+        //Layout created in Java Activity code
+        LinearLayout linearL = new LinearLayout(this);
+        linearL.setOrientation(LinearLayout.VERTICAL);
+
+        TextView myText = new TextView(this);
+        myText.setText("Display this text!");
+
+        linearL.addView(myText);
+        setContentView(linearL);
     }
 }
